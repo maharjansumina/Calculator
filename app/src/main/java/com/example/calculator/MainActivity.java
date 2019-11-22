97d99ce2451710afd9248cdf6dc566a61a51a625
sumina <suminamaharjan098@gmail.com>
2019-11-22 10:47:15 +0545

    Code Added and Change in UI

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,13 +3,20 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText etResult;
-    Button btnOne, btnTwo, btnThree, btnAdd, btnFour, btnFive, btnSix, btnSub, btnSeven,btnEight,btnNine, btnMul, btnDot, btnZero, btnDiv, btnEqual, btnClear;
+    Button btnOne, btnTwo, btnThree, btnAdd, btnFour, btnFive, btnSix, btnSeven,
+            btnEight,btnNine, btnMul, btnSub, btnDot, btnZero, btnDiv, btnEqual, btnClear;
+
+    float mValueOne , mValueTwo ;
+
+    boolean mAddition , mSubtract ,mMultiplication ,mDivision ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,5 +42,163 @@ public class MainActivity extends AppCompatActivity {
         btnDiv = findViewById(R.id.btnDiv);
         btnEqual = findViewById(R.id.btnEqual);
         btnClear = findViewById(R.id.btnClear);
+
+        btnOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etResult.setText(etResult.getText()+"1");
+            }
+        });
+
+        btnTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etResult.setText(etResult.getText()+"2");
+            }
+        });
+
+        btnThree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etResult.setText(etResult.getText()+"3");
+            }
+        });
+
+        btnFour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etResult.setText(etResult.getText()+"4");
+            }
+        });
+
+        btnFive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etResult.setText(etResult.getText()+"5");
+            }
+        });
+
+        btnSix.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etResult.setText(etResult.getText()+"6");
+            }
+        });
+
+        btnSeven.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etResult.setText(etResult.getText()+"7");
+            }
+        });
+
+        btnEight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etResult.setText(etResult.getText()+"8");
+            }
+        });
+
+        btnNine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etResult.setText(etResult.getText()+"9");
+            }
+        });
+
+        btnZero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etResult.setText(etResult.getText()+"0");
+            }
+        });
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (etResult == null){
+                    etResult.setText("");
+                }else {
+                    mValueOne = Float.parseFloat(etResult.getText() + "");
+                    mAddition = true;
+                    etResult.setText(null);
+                }
+            }
+        });
+
+        btnSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mValueOne = Float.parseFloat(etResult.getText() + "");
+                mSubtract = true ;
+                etResult.setText(null);
+            }
+        });
+
+        btnMul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mValueOne = Float.parseFloat(etResult.getText() + "");
+                mMultiplication = true ;
+                etResult.setText(null);
+            }
+        });
+
+        btnDiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mValueOne = Float.parseFloat(etResult.getText()+"");
+                mDivision = true ;
+                etResult.setText(null);
+            }
+        });
+
+        btnEqual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mValueTwo = Float.parseFloat(etResult.getText() + "");
+
+                if (mAddition == true){
+
+                    etResult.setText(mValueOne + mValueTwo +"");
+                    mAddition=false;
+                }
+
+
+                if (mSubtract == true){
+                    etResult.setText(mValueOne - mValueTwo+"");
+                    mSubtract=false;
+                }
+
+                if (mMultiplication == true){
+                    etResult.setText(mValueOne * mValueTwo+"");
+                    mMultiplication=false;
+                }
+
+                if (mDivision == true){
+                    etResult.setText(mValueOne / mValueTwo+"");
+                    mDivision=false;
+                }
+            }
+        });
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etResult.setText("");
+            }
+        });
+
+        btnDot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etResult.setText(etResult.getText()+".");
+            }
+        });
+
+
+
+
     }
 }
